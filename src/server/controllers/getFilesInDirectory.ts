63cfd091d3659639764529b58f4a940737f7ccc5
Gumer Shagiyanov <>
2019-10-08 21:54:06 +0300

    getFilesInDirectory to Typescript

--- src/server/controllers/getFilesInDirectory.ts
+++ src/server/controllers/getFilesInDirectory.ts
@@ -1,16 +1,17 @@
-const {exec} = require('child_process');
+import * as child from "child_process";
+import {ExecException} from "child_process";
 
-const getFilesInDirectory = (commitHash, repositoryPath, filePath) => {
-    return new Promise((resolve, reject) => {
-        exec(
+const getFilesInDirectory = (commitHash: string, repositoryPath: string, filePath: string) => {
+    return new Promise((resolve: (contentList: Array<object>) => void, reject: (error: object)=> void) => {
+        child.exec(
             `git show ${commitHash}:${'.'.concat(filePath)}`,
             {cwd: repositoryPath},
-            (err, content) => {
+            (err: ExecException | null, content: string) => {
                 if (err) {
                     reject({error: 'Error'});
                     return;
                 }
-                const contentList = content.split('\n\n')[1].split('\n');
+                const contentList: Array<string> = content.split('\n\n')[1].split('\n');
                 resolve(contentList.filter(id => id).map(id => {
                         id = id.lastIndexOf('/') > -1 ? id.slice(0, id.lastIndexOf('/')) : id;
                         return {
