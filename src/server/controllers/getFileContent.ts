5a088a5fb7f52a41a7d9ff22b6b7d729a7842e6e
Gumer Shagiyanov <>
2019-10-08 21:50:22 +0300

    getFileContent to Typescript

---
+++ src/server/controllers/getFileContent.ts
@@ -0,0 +1,20 @@
+import * as child from "child_process";
+import {ExecException} from "child_process";
+
+
+const getFileContent = (commitHash: string, repositoryPath: string, filePath: string) => {
+    return new Promise((resolve: (content: Array<string>) => void, reject: (error: object) => void) => {
+        child.exec(
+            `git show ${commitHash}:${'.'.concat(filePath)}`,
+            {cwd: repositoryPath},
+            (err: ExecException | null, content: string) => {
+                if (err) {
+                    reject({error: 'Error'});
+                    return;
+                }
+               resolve(content.split('\n'));
+            }
+        );
+    })
+};
+module.exports = {getFileContent};
